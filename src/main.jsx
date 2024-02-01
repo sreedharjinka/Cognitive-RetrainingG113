b96d735730da0bdac7af1a406698aa62c6daad4d
SasukeUchiha-05 <147145960+SasukeUchiha-05@users.noreply.github.com>
2024-02-01 19:37:30 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { AuthProvider } from './Acontext.jsx'
+// import { AuthProvider } from './Acontext.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <AuthProvider>
+
     <React.StrictMode>
       <App />
-    </React.StrictMode>
-  </AuthProvider>,
+    </React.StrictMode>,
+
   document.getElementById('root')
   
 )
