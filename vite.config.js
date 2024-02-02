cffbc3031a75face6c5357786cc1eb263551dbc7
SasukeUchiha-05 <147145960+SasukeUchiha-05@users.noreply.github.com>
2024-02-02 11:49:52 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base:"/cognitive-disabilities/",
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ["@fortawesome/react-fontawesome"],
+    },
+  },
+});
+
+
